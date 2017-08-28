a552a0c26388cbef5fce9bd4472ff154ebcd3964
Cameron <gnikgg@hotmail.com>
2017-08-28 02:43:55 -0700

    Reverted Alert changes

--- src/plugins/ALERT.php
+++ src/plugins/ALERT.php
@@ -1,50 +1,44 @@
 <?php
 class ALERT {
 	
-	private static $_alertType = $_SESSION;
-
 	public static function use($type = $_SESSION) {
-		self::$_alertType = $type;
+		self::$_SESSION = $type;
 	}
 
 	public static function error($input) {	
-		self::$_alertType['Error'] = $input;
+		self::$_SESSION['Error'] = $input;
 	}
 	
 	public static function success($input) {	
-		self::$_alertType['Success'] = $input;
+		self::$_SESSION['Success'] = $input;
 	}
 
 	public static function warning($input) {	
-		self::$_alertType['Warning'] = $input;
+		self::$_SESSION['Warning'] = $input;
 	}
 
 	public static function notice($input) {	
-		self::$_alertType['Notice'] = $input;
+		self::$_SESSION['Notice'] = $input;
 	}
 
 	public static function Validate() {
-		if (isset(self::$_alertType['Error']) && self::$_alertType['Error'] != "")
+		if (isset(self::$_SESSION['Error']) && self::$_SESSION['Error'] != "")
 			return false;
 		else
 			return true;
 	}
 
 	public static function check($alert) {
-		if (isset(self::$_alertType[$alert]))
+		if (isset(self::$_SESSION[$alert]))
 			return true;
 		else
 			return false;
 	}
 
 	public static function get($alert) {
-		if (isset(self::$_alertType[$alert])) {
-			$var = self::$_alertType[$alert];
-			unset(self::$_alertType[$alert]);
+		if (isset(self::$_SESSION[$alert])) {
+			$var = self::$_SESSION[$alert];
+			unset(self::$_SESSION[$alert]);
 			return $var;
 		} else { return false; }
 	}
-/*
-*	if (!ALERT::Validate())
-*/
-}
